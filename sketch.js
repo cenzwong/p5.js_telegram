c300ac5f15741fbd911c4a866b9fff0f7c82eeea
Cenz Wong <44856918+cenzwong@users.noreply.github.com>
2019-11-13 22:35:41 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,66 +1,205 @@
+//commenmt
+
 let capture;
 let BotID = 'bot941539918:AAEA-WBRpCLWYgPKokrjsdJqjoykIe2CElc'
+let urlPhoto = 'https://api.telegram.org/bot941539918:AAEA-WBRpCLWYgPKokrjsdJqjoykIe2CElc/sendPhoto'
 
 function setup() {
   // createCanvas(480, 480);
+  c = createCanvas(720, 480);
   capture = createCapture(VIDEO);
-  capture.hide();
-  c = createCanvas(1280, 720);
-  
+  // capture.hide();
+ 
+
 }
 
-function mouseClicked(){
-  console.log(capture.get());
+function mouseClicked() {
+  // console.log(capture.get());
+  let snapshot = image(capture.get(), 0, 0)
+  // saveCanvas(c, 'jpg');
   // image(capture, 0, 0, width, width * capture.height / capture.width);
-  image(capture.get(), 0, 0, capture.width, capture.height);
-  // image(img, x, y, [width], [height])
-  //tgSendMessage(BotID,'@testtp','HelloThereP5Function');
-  // tgSendPhoto(BotID,'@testtp',capture.get());
-  saveFrames('out', 'jpg', 0, 0, ImageSnapshot => { 
-    // console.log(ImageSnapshot[0]);
-   ImageData = String(ImageSnapshot[0].imageData);
-    // console.log(multipartFormData(ImageData));
-    tgSendPhoto(BotID,'@testtp',multipartFormData(ImageData));
-  });
+  // image(capture.get(), 0, 0, capture.width, capture.height);
+  // // image(img, x, y, [width], [height])
+  // tgSendMessage_GET(BotID, '@testtp', '===== I am the beautiful Seperation Line =====');
+  // tgSendMessage_GET(BotID, '@testtp', 'tgSendMessage_GET');
+  // tgSendMessage_POST_json(BotID, '@testtp', 'tgSendMessage_POST_json');
+  // tgSendMessage_POST_xhr(BotID, '@testtp', 'tgSendMessage_POST_xhr');
+  ////////tgSendMessage_POST_json2(BotID, '@testtp', 'Hello PoPo');
   
+  //tgSendPhoto_POST_xhr(BotID, '@testtp', PhotoURI);
+  // // tgSendPhoto(BotID,'@testtp',capture.get());
+  // saveFrames('out', 'jpg', 1, 1);
+  // saveFrames('out', 'jpg', 1, 1, ImageSnapshot => { 
+  //   // console.log(ImageSnapshot[0]);
+  //  ImageData = String(ImageSnapshot[0].imageData);
+  //   // console.log(multipartFormData(ImageData));
+  //   tgSendPhoto_POST_xhr(BotID,'@testtp',ImageData);
+  // });
+  test();
 }
 
 function draw() {
 
 }
 
-function tgSendMessage(tgbotToken, tgchatID, message){
-    let url = 'https://api.telegram.org/'+tgbotToken+'/sendMessage?chat_id='+tgchatID+'&text='+message;
-      httpGet(url, function(response) {
+function tgSendMessage_GET(tgbotToken, tgchatID, message) {
+  let url = 'https://api.telegram.org/' + tgbotToken + '/sendMessage?chat_id=' + tgchatID + '&text=' + message;
+  httpGet(url, function(response) {
     // when the HTTP request completes, populate the variable that holds the
     // earthquake data used in the visualization.
-    console.log(response);
+    console.log(JSON.parse(response));
   });
 }
 
-function tgSendPhoto(tgbotToken, tgchatID, imageFormData){
-
-  let url = 'https://api.telegram.org/'+tgbotToken+'/sendMessage?chat_id='+tgchatID;
-  // console.log(String(imageFormData.content));
-  httpDo(
-      url,
-      {
-        method: 'POST',
-        // Other Request options, like special headers for apis
-        content: imageFormData.content , //FormData
-        headers: imageFormData.headers
-      },
-      function(res) {
-        console.log(res);
-      }
-    );
+function tgSendMessage_POST_json(tgbotToken, tgchatID, message) {
+  let url = 'https://api.telegram.org/' + tgbotToken + '/sendMessage';
+  httpPost(
+    url,
+    'json', {
+      chat_id: tgchatID,
+      text: message
+    },
+    function(result) {
+      console.log(result);
+    }
+  );
 }
 
+function tgSendMessage_POST_xhr(tgbotToken, tgchatID, message) {
+  var data = new FormData();
+  data.append("text", message);
+  data.append("chat_id", tgchatID);
+
+  var xhr = new XMLHttpRequest();
+
+  xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === 4) {
+      console.log(JSON.parse(this.responseText));
+    }
+  });
+
+  let URL = "https://api.telegram.org/"+tgbotToken+"/sendMessage";
+  xhr.open("POST", URL);
+
+  xhr.send(data);
+}
+
+function tgSendPhoto_POST_xhr(tgbotToken, tgchatID, PhotoURI) {
+  var data = new FormData();
+
+  data.append("chat_id", tgchatID);
+  data.append("Photo", PhotoURI);
+  
+  var xhr = new XMLHttpRequest();
+
+  xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === 4) {
+      console.log(this.responseText);
+    }
+  });
+
+  let URL = "https://api.telegram.org/"+tgbotToken+"/sendPhoto";
+  xhr.open("POST", URL);
+
+  xhr.send(data);
+}
+
+function tgSendMessage_POST_json2(tgbotToken, tgchatID, message) {
+  // Fail to make POST request
+  let url = 'https://api.telegram.org/' + tgbotToken + '/sendMessage';
+  httpDo(
+    url, {
+      method: 'POST',
+      // // Other Request options, like special headers for apis
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded,multipart/form-data; boundary=--------------------------200668755530089614109187",
+        "Accept": "application/json",
+        "Content-Length": "282"
+       },
+      content: {"chat_id": tgchatID, "text": message}
+    },
+    function(res) {
+      console.log(res);
+    }
+  );
+  
+  
+}
+
+
+function tgSendPhoto(tgbotToken, tgchatID, imageFormData) {
+
+  let url = 'https://api.telegram.org/' + tgbotToken + '/sendPhoto';
+  console.log(imageFormData.content);
+  httpDo(
+    url, {
+      method: 'POST',
+      // Other Request options, like special headers for apis
+      content: String(imageFormData.content), //FormData
+      headers: imageFormData.headers
+    },
+    function(res) {
+      console.log(res);
+    }
+  );
+}
+
+
+
+
+
+let img;
+
+
+function test() {
+  input = createFileInput(handleFile);
+  input.position(0, 0);
+}
+
+
+function handleFile(file) {
+  print(file);
+  // tgSendPhoto(BotID, '@testtp', multipartFormData(String(file.data)));
+  // if (file.type === 'image') {
+  //   img = createImg(file.data, '');
+  //   img.hide();
+  // } else {
+  //   img = null;
+  // }
+  let photoin = document.getElementById('photo');
+  var data = new FormData();
+  data.append("chat_id", "@testtp");
+  data.append("photo", photoin);
+ 
+  
+  var xhr = new XMLHttpRequest();
+  
+  xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === 4) {
+      console.log(this.responseText);
+    }
+  });
+  
+  xhr.open("POST", "https://api.telegram.org/bot941539918:AAEA-WBRpCLWYgPKokrjsdJqjoykIe2CElc/sendPhoto");
+  xhr.setRequestHeader("Content-Type", " multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW");
+  
+  xhr.send(data);
+
+
+
+}
+
+
+
+
+
+
+
 //=========== Library =======================
-function doHTTPGet(){
-//   httpGet(path, [datatype], [data], [callback], [errorCallback])
-// httpGet(path, data, [callback], [errorCallback])
-// httpGet(path, callback, [errorCallback])
+function doHTTPGet() {
+  //   httpGet(path, [datatype], [data], [callback], [errorCallback])
+  // httpGet(path, data, [callback], [errorCallback])
+  // httpGet(path, callback, [errorCallback])
   //pre-installed function
   let url = 'https://api.telegram.org/bot941539918:AAEA-WBRpCLWYgPKokrjsdJqjoykIe2CElc/sendMessage?chat_id=@testtp&text=HelloFromBotandFromP5';
   httpGet(url, function(response) {
@@ -70,31 +209,34 @@ function doHTTPGet(){
   });
 }
 
-function doHTTPPost(){
+function doHTTPPost() {
   let url = 'https://jsonplaceholder.typicode.com/posts';
   httpPost(
     url,
-    'json',
-    {name: 1, title: 'HelloWorld'},
-    function(result){
+    'json', {
+      name: 1,
+      title: 'HelloWorld'
+    },
+    function(result) {
       console.log(result);
     }
   );
 }
 
-function doHTTPDoPost(){
+function doHTTPDoPost() {
   saveFrames('out', 'png', 1, 25, data => {
     //print(data);
   });
-  let url = 'https://api.telegram.org/'+tgbotToken+'/sendMessage?chat_id='+tgchatID;
+  let url = 'https://api.telegram.org/' + tgbotToken + '/sendMessage?chat_id=' + tgchatID;
   console.log(data);
   httpDo(
-    url,
-    {
+    url, {
       method: 'POST',
       // Other Request options, like special headers for apis
-      body: formdata ,
-      headers: { authorization: 'Bearer secretKey' }
+      body: formdata,
+      headers: {
+        authorization: 'Bearer secretKey'
+      }
     },
     function(res) {
       earthquakes = res;
@@ -102,25 +244,34 @@ function doHTTPDoPost(){
   );
 }
 
-function multipartFormData (inImage) {
-  var boundary = '----'+(new Date()).getTime();
+function multipartFormData(inImage) {
+  var boundary = '----' + (new Date()).getTime();
   var bodyString = [];
   bodyString.push(
     '--' + boundary,
-    'Content-Disposition: form-data; name="' + "file" + '";' + 'filename="' + "my_file.jpg" + '"',
-	'Content-Type: ' + "image/octet-stream",
-	'Content-Transfer-Encoding: base64','', //need /r/n twice here
-	 inImage.substring(31) //remove the data:image/jpeg;base64,
-  );  
-  
-  bodyString.push('--' + boundary + '--','');
+    'Content-Disposition: form-data; name="' + "photo" + '";' + 'filename="' + "my_file.jpg" + '"',
+    'Content-Type: ' + "image/octet-stream",
+    'Content-Transfer-Encoding: base64', '', //need /r/n twice here
+    inImage.substring(31) //remove the data:image/jpeg;base64, 23
+  );
+
+  bodyString.push(
+    '--' + boundary,
+    'Content-Disposition: form-data; name="' + 'chat_id"',
+    '', //need /r/n twice here
+    '@testtp' //remove the data:image/jpeg;base64,
+  );
+
+  bodyString.push('--' + boundary + '--', '');
+
+
 
   var content = bodyString.join('\r\n');
   return {
     content: content,
     headers: {
-      'Content-Type': 'multipart/form-data; boundary='+boundary,
-		'Content-Length': content.length
+      'Content-Type': 'multipart/form-data; boundary=' + boundary,
+      'Content-Length': content.length
     }
   }
 }
